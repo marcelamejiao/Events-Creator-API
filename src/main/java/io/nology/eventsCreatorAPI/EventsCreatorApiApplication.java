d5cbe73c3c9d96322080979a9dcbcdf33cb2220a
marcelamejiao <mejiao24@hotmail.com>
2023-12-22 16:07:19 +1100

    initial commit

---
+++ src/main/java/io/nology/eventsCreatorAPI/EventsCreatorApiApplication.java
@@ -0,0 +1,13 @@
+package io.nology.eventsCreatorAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsCreatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsCreatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/nology/eventsCreatorAPI/EventsCreatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.eventsCreatorAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsCreatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
