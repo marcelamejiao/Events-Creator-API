7b34a28640d6e18efbc5ae4fccd401111f88ab86
marcelamejiao <mejiao24@hotmail.com>
2023-12-27 13:16:38 +1100

    implement DTO to update an event

---
+++ src/main/java/io/nology/eventsCreatorAPI/event/EventUpdateDTO.java
@@ -0,0 +1,40 @@
+package io.nology.eventsCreatorAPI.event;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import java.text.ParseException;
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+public class EventUpdateDTO {
+
+    @NotBlank
+    private String eventName;
+
+    @NotBlank
+    private OffsetDateTime startDate;
+
+    @NotBlank
+    private OffsetDateTime endDate;
+
+    @NotBlank
+    private String location;
+
+    @NotBlank
+    private String label;
+
+    public EventUpdateDTO(String eventName,
+                          OffsetDateTime startDate,
+                          OffsetDateTime endDate,
+                          String  location,
+                          String label)  throws ParseException {
+        this.eventName = eventName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.label = label;
+    }
+
+}
