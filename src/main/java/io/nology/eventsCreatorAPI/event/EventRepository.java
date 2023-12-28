9cbfa2c1e652cc90fd6daa97c1bb1904ca115d94
marcelamejiao <mejiao24@hotmail.com>
2023-12-28 12:00:09 +1100

    implement methods in the respository layer to find events using label or location

--- src/main/java/io/nology/eventsCreatorAPI/event/EventRepository.java
+++ src/main/java/io/nology/eventsCreatorAPI/event/EventRepository.java
@@ -2,5 +2,11 @@ package io.nology.eventsCreatorAPI.event;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
+
+    List<Event> findByLabel(String label);
+
+    List<Event> findByLocation(String location);
 }
